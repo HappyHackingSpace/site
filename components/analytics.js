f2df74e529d7df8d857baa889be868186a5bc436
arjin albay <arjinalbay21@gmail.com>
2025-12-25 20:42:50 +0300

    refactor Analytics component to integrate Google Analytics tracking

--- components/analytics.js
+++ components/analytics.js
@@ -1,11 +1,32 @@
 import Script from 'next/script'
 
-const Analytics = () => (
-  <Script
-    defer
-    data-domain="hackclub.com"
-    src="https://plausible.io/js/script.pageview-props.tagged-events.js"
-  />
-)
+const Analytics = () => {
+  const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
+
+  if (!GA_MEASUREMENT_ID) {
+    return null
+  }
+
+  return (
+    <>
+      {/* Google tag (gtag.js) */}
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}');
+          `,
+        }}
+      />
+    </>
+  )
+}
 
 export default Analytics
